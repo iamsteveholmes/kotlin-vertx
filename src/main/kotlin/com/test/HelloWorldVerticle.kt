c6442b2ede1aaa1e675fb4435b49b1b8fb60b2fd
Steve Holmes <steven.holmes@ucdmc.ucd.edu>
2015-03-02 16:49:26 -0800

    using Apex router

--- src/main/kotlin/com/test/HelloWorldVerticle.kt
+++ src/main/kotlin/com/test/HelloWorldVerticle.kt
@@ -2,10 +2,20 @@ package com.test
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
+import io.vertx.ext.apex.Router
 
-class HelloWorldVerticle : AbstractVerticle() {
+class HelloWorldVerticle: AbstractVerticle() {
 
     override fun start() {
-        Vertx.vertx().createHttpServer().requestHandler({request -> request.response().end("Hello Kotlin World!")}).listen(8080);
+        val router = router(vertx)
+
+        router.route().handler({ routingContext ->
+            routingContext.response().putHeader("content-type", "text/html").end("Hello Apex Kotlin World!")
+        })
+
+        vertx.createHttpServer().requestHandler({request -> router.accept(request)}).listen(8080);
+
     }
 }
+
+fun router(vertx: Vertx): Router = Router.router(vertx)
